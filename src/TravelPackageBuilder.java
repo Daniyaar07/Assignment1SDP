a6ef94be3969f5d67751d39a524bb97230c9f598
Daniyar <daniyarbaikanov07@gmail.com>
2026-09-11 19:29:38 +0500

    add travelpackagebuilder interface

--- src/TravelPackageBuilder.java
+++ src/TravelPackageBuilder.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
-public class TravelPackageBuilder {
+public interface TravelPackageBuilder {
+    TravelPackageBuilder setDestination(String destination);
+    TravelPackageBuilder setHotel(String hotel);
+    TravelPackageBuilder setTransport(String transport);
+    TravelPackageBuilder setDays(int days);
+    TravelPackageBuilder setMealsIncluded(boolean mealsIncluded);
+    TravelPackageBuilder build();
 }
