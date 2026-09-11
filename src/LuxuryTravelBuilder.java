5a865fb714a36be6bb4ff1b1b26fdbb86fe6a2fa
Daniyar <daniyarbaikanov07@gmail.com>
2026-09-11 20:11:40 +0500

    add basic LuxuryTravelBuilder

--- src/LuxuryTravelBuilder.java
+++ src/LuxuryTravelBuilder.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
-public class LuxuryTravelBuilder {
+public class LuxuryTravelBuilder implements TravelPackageBuilder{
+    private TravelPackage travelPackage = new TravelPackage();
+    @Override
+    public TravelPackageBuilder setDestination(String destination){
+        travelPackage.setDestination(destination);
+        return this;
+    }
+    @Override
+    public TravelPackageBuilder setHotel(String hotel){
+        travelPackage.setHotel(hotel);
+        return this;
+    }
+    @Override
+    public TravelPackageBuilder setTransport(String transport){
+        travelPackage.setTransport(transport);
+        return this;
+    }
 }
