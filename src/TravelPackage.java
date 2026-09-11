1bbcbf6fc7745dda8632a10c60a7ee361bf6bdb4
Daniyar <daniyarbaikanov07@gmail.com>
2026-09-11 18:43:47 +0500

    Add TravelPackage constructor

--- src/TravelPackage.java
+++ src/TravelPackage.java
@@ -5,4 +5,11 @@ public class TravelPackage {
     private int days;
     private boolean mealsIncluded;
 
+    public TravelPackage(String destination, String hotel , String transport , int days , boolean mealsIncluded){
+        this.destination = destination;
+        this.hotel = hotel;
+        this.transport = transport;
+        this.days = days;
+        this.mealsIncluded = mealsIncluded;
+    }
 }
