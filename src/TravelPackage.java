e6ea7feec1af5fd6efdfa267ec8c90a5b76ff8c3
Daniyar <daniyarbaikanov07@gmail.com>
2026-09-11 19:43:18 +0500

    add setters

--- src/TravelPackage.java
+++ src/TravelPackage.java
@@ -5,11 +5,21 @@ public class TravelPackage {
     private int days;
     private boolean mealsIncluded;
 
-    public TravelPackage(String destination, String hotel , String transport , int days , boolean mealsIncluded){
+    public TravelPackage(){}
+
+   public void setDestination(String destination){
         this.destination = destination;
+   }
+   public void setHotel(String hotel){
         this.hotel = hotel;
+   }
+    public void setTransport(String transport){
         this.transport = transport;
+    }
+    public void setDays(int days){
         this.days = days;
+    }
+    public void setMealsIncluded(boolean mealsIncluded){
         this.mealsIncluded = mealsIncluded;
     }
     public String getDestination(){
