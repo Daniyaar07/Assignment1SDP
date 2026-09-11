4f60afbd4913a919cebab7687dc6b0faa82841b3
Daniyar <daniyarbaikanov07@gmail.com>
2026-09-11 18:38:34 +0500

    add travelpackage fields

---
+++ src/TravelPackage.java
@@ -0,0 +1,8 @@
+public class TravelPackage {
+    private String destination;
+    private String hotel;
+    private String transport;
+    private int days;
+    private boolean mealsIncluded;
+
+}
