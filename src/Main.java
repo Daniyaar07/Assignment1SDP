0ac82bf8bcecda420938dac2d86a143c57db0679
Daniyar <daniyarbaikanov07@gmail.com>
2026-09-11 20:39:27 +0500

    add main demo for travel

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args){
+        TravelDirector director = new TravelDirector();
+        TravelPackageBuilder budgetBuider = new BudgetTravelBuilder();
+        TravelPackage budgetTrip = director.createBudgetTrip(budgetBuider);
 
+        TravelPackageBuilder luxuryBuilder = new LuxuryTravelBuilder();
+        TravelPackage luxuryTrip = director.createLuxuryTrip(luxuryBuilder);
+
+        System.out.println("Budget travel:");
+        System.out.println(budgetTrip);
+
+        System.out.println();
+
+        System.out.println("Luxury travel:");
+        System.out.println(luxuryTrip);
     }
 }
